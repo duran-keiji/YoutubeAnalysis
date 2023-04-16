6b946564be1acd6433fa52b74cb67e4cb1b73497
duran_keiji <>
2023-04-17 03:19:16 +0900

    動くものをpush(チャンネルIDと動画IDと視聴回数と登録者数取得（ver2）)

--- lambda_go/golang-sam-sample/youtube-search-by-keyword/main.go
+++ lambda_go/golang-sam-sample/youtube-search-by-keyword/main.go
@@ -1,5 +1,3 @@
-// チャンネルIDと動画IDと視聴回数と登録者数取得（ver2）
-
 package main
 
 import (
@@ -17,6 +15,7 @@ import (
 
 const APIKey = "AIzaSyDAKFWvZl0y9DQI47F2tEaJPncMvnDPdkE"
 
+
   // [1] 構造体のフィールドとJSONのキーを紐付け
 type Search struct {
 	NextPageToken       string    `json:"nextPageToken"`
@@ -33,19 +32,12 @@ type SearchID struct {
 	VideoId string `json:"videoId"`
 }
 
-
 type SearchSnippet struct {
 	ChannelId string `json:"channelId"`
 	Title string `json:"title"`
 }
 
-type Data struct {
-	ChannelId string `json:"channelId"`
-	VideoId  string    `json:"videoId"`
-	ViewCount  string    `json:"viewCount"`
-	SubscriberCount  string    `json:"subscriberCount"`
-}
-func get_youtube_data() string {
+func get_youtube_id(APIKey string) (string, string) {
 	url := "https://www.googleapis.com/youtube/v3/search"
  
 	request, err := http.NewRequest("GET", url, nil)
@@ -82,74 +74,22 @@ func get_youtube_data() string {
 	}
 	fmt.Println("body=", string(body))
 
-	// [3] 配列型のJSONデータを読み込む
-	// FIXME: objが取得できていない
-	// obj := make([]*Search, 2)
-	var obj []*Search
-	
-	// obj := make([]*Search, 0)
-	// obj := []*Search{}
-	fmt.Printf("全 %d 件\n\n", obj)
-	json.Unmarshal(body, &obj)
-	fmt.Printf("全 %d 件\n\n", obj)
+	var search Search
+	json.Unmarshal(body, &search)
+	fmt.Println("ChannelId=", search.Items[0].Snippet.ChannelId)
+	fmt.Println("VideoId=", search.Items[0].ID.VideoId)
 
-	// スライスの要素数を取得
-	// fmt.Printf("全 %d 件\n\n", len(obj))
-
-	var ChannelId string
-	var VideoId string
-	var ViewCount string
-	var SubscriberCount string
-	var data []*Data
-
-	// スライスの要素を順に取り出す
-	for _, search := range obj {
-		if len(search.Items) != 0 {
-			for i, items := range search.Items {
-				ChannelId = items.Snippet.ChannelId
-				VideoId = items.ID.VideoId
-				ViewCount = get_youtube_viewCount(string(VideoId))
-				SubscriberCount = get_youtube_subscriberCount(string(ChannelId))
-				fmt.Printf(string(i),ChannelId,VideoId,ViewCount,SubscriberCount)
-				// obj[i].ChannelId = ChannelId
-				// obj[i]["VideoId"] = VideoId
-				// obj[i]["ViewCount"] = ViewCount
-				// obj[i]["SubscriberCount"] = SubscriberCount
-				data = append(data, &Data{ChannelId: string(ChannelId), VideoId: string(VideoId), ViewCount: string(ViewCount), SubscriberCount: string(SubscriberCount)})
-			}
-		}
-	} 
-	fmt.Printf("respons=", data)
-
-	res, err := json.Marshal(data)
+	ChannelId, err := json.Marshal(search.Items[0].Snippet.ChannelId)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return string(res)
+	VideoId, err := json.Marshal(search.Items[0].ID.VideoId)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-
-	// var search Search
-	// json.Unmarshal(body, &search)
-	// fmt.Println("ChannelId=", search.Items[0].Snippet.ChannelId)
-	// fmt.Println("VideoId=", search.Items[0].ID.VideoId)
-
-	
-
-	// ChannelId, err := json.Marshal(search.Items[0].Snippet.ChannelId)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// VideoId, err := json.Marshal(search.Items[0].ID.VideoId)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// ViewCount := get_youtube_viewCount(string(ChannelId), string(VideoId))
-	// SubscriberCount := get_youtube_subscriberCount(string(ChannelId))
-
-	// return string(ChannelId), string(VideoId)
+	return string(ChannelId), string(VideoId)
 }
 
 
@@ -168,7 +108,7 @@ type VideoStatistic struct {
 	ViewCount string `json:"viewCount"`
 }
 
-func get_youtube_viewCount(VideoId string) string {
+func get_youtube_viewCount(APIKey string, videoId string) string {
 
 	url := "https://www.googleapis.com/youtube/v3/videos"
  
@@ -177,15 +117,15 @@ func get_youtube_viewCount(VideoId string) string {
 		log.Fatal(err)
 	}
 
-	// なぜかVideoIdにダブルクォートが含まれるためダブルクォートをカットする。これをしないとparams.Add("id", VideoId)のvideoIdでダブルクォート込みで検索してしまい値が取れない。
-	if strings.Contains(VideoId, "\"") {
-		VideoId = strings.Replace(VideoId, "\"", "", -1)
+	// なぜかvideoIdにダブルクォートが含まれるためダブルクォートをカットする。これをしないとparams.Add("id", videoId)のvideoIdでダブルクォート込みで検索してしまい値が取れない。
+	if strings.Contains(videoId, "\"") {
+		videoId = strings.Replace(videoId, "\"", "", -1)
 	}
 	
 	//クエリパラメータ
 	params := request.URL.Query()
 	params.Add("key", APIKey)
-	params.Add("id", VideoId)
+	params.Add("id", videoId)
 	params.Add("part", "statistics") 
 	params.Add("maxResults", "2")
 
@@ -209,7 +149,7 @@ func get_youtube_viewCount(VideoId string) string {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// fmt.Println("body=", string(body))
+	fmt.Println("body=", string(body))
 
 	var video Video
 	json.Unmarshal(body, &video)
@@ -222,8 +162,6 @@ func get_youtube_viewCount(VideoId string) string {
 	}
 
 	return string(ViewCount)
-
-	// return string(ViewCount)
 }
 
 // [1] 構造体のフィールドとJSONのキーを紐付け
@@ -241,7 +179,7 @@ type ChannelStatistic struct {
 	SubscriberCount string `json:"subscriberCount"`
 }
 
-func get_youtube_subscriberCount(ChannelId string) string {
+func get_youtube_subscriberCount(APIKey string, channelId string) string {
 
 	url := "https://www.googleapis.com/youtube/v3/channels"
  
@@ -251,14 +189,14 @@ func get_youtube_subscriberCount(ChannelId string) string {
 	}
 
 	// なぜかchannelIdにダブルクォートが含まれるためダブルクォートをカットする。これをしないとparams.Add("id", channelId)のchannelIdでダブルクォート込みで検索してしまい値が取れない。
-	if strings.Contains(ChannelId, "\"") {
-		ChannelId = strings.Replace(ChannelId, "\"", "", -1)
+	if strings.Contains(channelId, "\"") {
+		channelId = strings.Replace(channelId, "\"", "", -1)
 	}
 	
 	//クエリパラメータ
 	params := request.URL.Query()
 	params.Add("key", APIKey)
-	params.Add("id", ChannelId)
+	params.Add("id", channelId)
 	params.Add("part", "statistics") 
 	params.Add("maxResults", "2")
 
@@ -282,7 +220,7 @@ func get_youtube_subscriberCount(ChannelId string) string {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// fmt.Println("body=", string(body))
+	fmt.Println("body=", string(body))
 
 	var channel Channel
 	json.Unmarshal(body, &channel)
@@ -299,11 +237,13 @@ func get_youtube_subscriberCount(ChannelId string) string {
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-	data := get_youtube_data()
+	channelId, videoId := get_youtube_id(APIKey)
+	viewCount := get_youtube_viewCount(APIKey, videoId)
+	subscriberCount := get_youtube_subscriberCount(APIKey, channelId)
 
 	return events.APIGatewayProxyResponse{
  
-		 Body:       fmt.Sprintf(data),
+		 Body:       fmt.Sprintf(channelId, videoId, viewCount, subscriberCount),
 		 StatusCode: 200,
 	 }, nil
  }
